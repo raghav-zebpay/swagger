1eb7a2f0b86608068ed496f153b2700e8dedb0d7
Raghav <raghav.g@zebpay.com>
2023-01-27 14:48:29 +0530

    swagger documentation done for put request

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const morgan = require("morgan");
+// const low = require("lowdb");
 const swaggerJSDoc = require('swagger-jsdoc')
 const swaggerUI = require('swagger-ui-express')
-const homeRouter = require("./routes/home")
+const bookRouter = require("./routes/book")
 
-const options = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "Testing",
-            version: "1.0.0",
-            description: " Learning swagger"
-        },
-        servers: [
-            {
-                url: "http://localhost:3000"
-            }
-        ],
-    },
-    apis: ["./routes/*.js"]
-}
+// import express from 'express'
+// import bodyParser from 'body-parser'
+// import cors from 'cors'
+// import morgan from "morgan"
+// import low from "lowdb"
+// import swaggerJSDoc from 'swagger-jsdoc'
+// import swaggerUI from 'swagger-ui-express'
+// import {bookRouter} from "./routes/book.js"
 
-const specs = swaggerJSDoc(options)
+// const FileSync = require("lowdb/adapters/FileSync");
 
 const app = express()
 app.use(bodyParser.json());
@@ -30,11 +24,33 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))
-
 app.use(cors({ origin: true }));
 
-app.use("/home", homeRouter)
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "ETH-HOUSE",
+            version: "1.0.0",
+            description: " Learning swagger"
+        },
+        servers: [
+            {
+                url: "http://localhost:3000"
+            },
+        ],
+    },
+    apis: ["./routes/*.js"]
+}
+
+const specs = swaggerJSDoc(options)
+
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))
+
+
+app.use("/books", bookRouter)
 
 app.get("/", function (req, res) {
     res.send("I am alive")
